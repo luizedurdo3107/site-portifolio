3b0a92e4482abb80e4eb703957f5ac025e3302ce
luizedurdo3107 <luizedurdo3107@gmail.com>
2026-05-08 15:06:56 +0000

    meu portifolio usando html,css e js

---
+++ conquistas.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+
+    document.querySelector("#texto").classList.add("mostrar");
+
+    document.querySelector(".foto-conquista")
+    .classList.add("mostrar-img");
+
+});
