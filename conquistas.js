df7dcae206f53a458c4eeaf06ae9144959719b42
luizedurdo3107 <luizedurdo3107@gmail.com>
2026-05-14 15:55:23 +0000

    htrthr

--- conquistas.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-
-    document.querySelector("#texto").classList.add("mostrar");
-
-    document.querySelector(".foto-conquista")
-    .classList.add("mostrar-img");
-
-});
